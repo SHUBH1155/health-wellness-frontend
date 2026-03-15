bbb28bb0078bf330882814a5682ce55d716d4c36
SHUBH1155 <shubhamchauhan0114@gmail.com>
2026-03-15 12:51:18 +0530

    add 1 change in api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
- baseURL:"https://health-wellness-backend-qt8p.onrender.com"
+ baseURL:"https://health-wellness-backend-qt8p.onrender.com/api"
 })
 
 API.interceptors.request.use((req)=>{
